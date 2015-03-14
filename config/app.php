7dadf528ac88e4567cfe646b806498c50db1c6b7
fideloper <fideloper@gmail.com>
2015-03-13 20:52:44 -0500

    cashier SP and migration to users table

--- config/app.php
+++ config/app.php
@@ -145,6 +145,11 @@ return [
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
 
+        /*
+         * Billing Service Providers...
+         */
+        'Laravel\Cashier\CashierServiceProvider',
+
 	],
 
 	/*
